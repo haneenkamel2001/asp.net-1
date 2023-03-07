8e63f443786f0d28c62ed4220d8d9257f0db6ea5
Haneen Kamel <116191117+haneenkamel2001@users.noreply.github.com>
2023-03-07 13:59:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System;
+
+var colorsArr = new string[] { "red-" };
+List<string> colors = new List<string>();
+Console.WriteLine(colors.Count);
+colors.Add("RED");
+colors.Add("BLACN");
+colors[5] = "ORANE";
+
+Colors.RemoveAt(3);
+Console.WriteLine(colors.Count);
+for (int i = 0; i < colors.Count; i++)
+{
+    Console.WriteLine(colors[1]);
+}
